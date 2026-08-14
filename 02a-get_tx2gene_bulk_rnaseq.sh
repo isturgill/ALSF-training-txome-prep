a1f0e0288754acbf2b7f0f1a064eb6e2f2f47fbb
isturgill <51013120+isturgill@users.noreply.github.com>
2026-08-14 15:34:53 -0400

    Add updated tx2gene Shell scripts for bulk RNA-seq and scRNA-seq

---
+++ 02a-get_tx2gene_bulk_rnaseq.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# If we want to use tximport (and we do for bulk RNA-seq!), we need a mapping between transcripts
+# and genes
+
+# make a directory to hold these tsv files
+mkdir -p tx2gene
+set -euo pipefail
+
+# all gtf files are in the same directory
+gtf_directory=data/gtf
+
+# Homo sapiens
+Rscript scripts/get_tx2gene_bulk_rnaseq.R \
+  --gtf_file ${gtf_directory}/Homo_sapiens.GRCh38.95.gtf \
+  --output_file tx2gene/Homo_sapiens.GRCh38.95_tx2gene.tsv
+
+# Mus musculus
+Rscript scripts/get_tx2gene_bulk_rnaseq.R \
+  --gtf_file ${gtf_directory}/Mus_musculus.GRCm38.95.gtf \
+  --output_file tx2gene/Mus_musculus.GRCm38.95_tx2gene.tsv
+
+# Danio rerio
+Rscript scripts/get_tx2gene_bulk_rnaseq.R \
+  --gtf_file ${gtf_directory}/Danio_rerio.GRCz11.95.gtf \
+  --output_file tx2gene/Danio_rerio.GRCz11.95_tx2gene.tsv
+
+# Canis familiaris
+Rscript scripts/get_tx2gene_bulk_rnaseq.R \
+  --gtf_file ${gtf_directory}/Canis_familiaris.CanFam3.1.95.gtf \
+  --output_file tx2gene/Canis_familiaris.CanFam3.1.95_tx2gene.tsv
